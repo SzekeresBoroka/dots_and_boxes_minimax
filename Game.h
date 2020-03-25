a7e582dbc3bbf33c2136f0397e75ca95bc5c74b6
SzekeresBoroka <szekeresboroka@gmail.com>
2020-03-25 15:05:40 +0200

    board explanation in Game.h

--- Game.h
+++ Game.h
@@ -4,6 +4,26 @@ class Game
 private:
 	int rows, cols;
 	int** board;
+	/*
+	BOARD 3x3 (5x5 actually) -> 2*rows - 1, 2*cols - 1
+
+				   0 1 2 3 4	-> actual coordinates
+				   0   1   2	-> coordinates for player
+				  ___________
+			0  0 | *   *   *
+			1    |
+			2  1 | *   *   *
+			3    |
+			4  2 | *   *   *
+
+
+			DOTS: -> i and j are even numbers
+
+			LINES: -> i is odd, j is even 
+					  or i is even, j is odd
+
+			SQUARES: -> i and j are odd numbers
+	*/
 public:
 	Game(const int rows, const int cols);
 	~Game();
