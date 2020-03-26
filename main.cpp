948edfd2894e6ebfc8baf60c4a73272f76f98e6f
beatafall <53474790+beatafall@users.noreply.github.com>
2020-03-26 13:15:13 +0200

    input arguments

--- main.cpp
+++ main.cpp
@@ -6,9 +6,40 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
 	cout << "Dots and Boxes - Minimax Algorithm" << endl << endl;
-	int rows = 3, cols = 3, depth = 10, turnTime = 10000;
+	int rows = 0, cols = 0, depth, turnTime;
+	bool inputDepth = false, inputTime = false;
+	for (int i = 1; i < argc; ++i) {
+		if (strcmp(argv[i], "-d") == 0) {
+			inputDepth = true;
+			depth = atoi(argv[i + 1]);
+			i++;
+			continue;
+		}
+		if (strcmp(argv[i], "-t") == 0) {
+			inputTime = true;
+			turnTime = atoi(argv[i + 1]);
+			i++;
+			continue;
+		}
+	}
+	while (rows < 2) {
+		cout << "Number of rows (min 2): ";
+		cin >> rows;
+	}
+	while (cols < 2) {
+		cout << "Number of columns (min 2): ";
+		cin >> cols;
+	}
+	if (!inputDepth) {
+		cout << "Depth of game tree: ";
+		cin >> depth;
+	}
+	if (!inputTime) {
+		cout << "Time of one turn (in milliseconds): ";
+		cin >> turnTime;
+	}
 	Game *game = new Game(rows, cols);
 	HumanPlayer *p1 = new HumanPlayer(game);
 	ComputerPlayer *p2 = new ComputerPlayer(game, depth, turnTime);
